133a244af84c466ee09be46cc59d06342c077660
AmritKadel <amritkadel10@gmail.com>
2024-05-07 22:24:53 +0545

    Last Commit

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//jshint esversion: 6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
@@ -7,20 +5,20 @@ const https = require("https");
 
 const app = express();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
-app.get("/",function(req,res){
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req,res){
+app.post("/", function (req, res) {
   const firstName = req.body.fname;
   const lastName = req.body.lname;
   const email = req.body.email;
   const data = {
-    members : [
+    members: [
       {
         email_address: email,
-        status : "subscribed",
+        status: "subscribed",
         merge_fields: {
           FNAME: firstName,
           LNAME: lastName
@@ -29,32 +27,32 @@ app.post("/", function(req,res){
     ]
   };
 
-const jsonData = JSON.stringify(data);
-const url = "https://us1.api.mailchimp.com/3.0/lists/7e254304d5";
-const options ={
-  method: "POST",
-  auth: "Sanskriti1:64045695ef4dcf8d0ecf542a86f3afc1-us1"
-}
-const request = https.request(url,options,function(response){
-  if(response.statusCode === 200){
-    res.sendFile(__dirname+ "/success.html");
-  } else{
-    res.sendFile(__dirname+ "/failure.html");
+  const jsonData = JSON.stringify(data);
+  const url = "https://us1.api.mailchimp.com/3.0/lists/7e254304d5";
+  const options = {
+    method: "POST",
+    auth: "Sanskriti1:64045695ef4dcf8d0ecf542a86f3afc1-us1"
   }
-  response.on("data",function(data){
-    console.log(JSON.parse(data));
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+
   });
-
-});
-request.write(jsonData);
-request.end();
+  request.write(jsonData);
+  request.end();
 });
 
-app.post("/failure",function(req,res){
+app.post("/failure", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(process.env.PORT ||3000,function(){
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000");
 });
 
